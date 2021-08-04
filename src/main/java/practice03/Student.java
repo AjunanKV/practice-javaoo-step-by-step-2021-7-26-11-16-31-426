f3b61f394a0e377b55bcc65e006c6a89c699874b
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-04 16:49:48 +0800

    [practice03_allTest][Kevin]update:After reading the read me it is said to use inheritance so add inhertiance in my project

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,30 +1,11 @@
 package practice03;
 
-public class Student {
-    private String name;
-    private int age;
+public class Student extends Person{
     private int Klass;
 
     public Student(String name, int age, int klass) {
-        this.name = name;
-        this.age = age;
+        super(name, age);
         Klass = klass;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public int getKlass() {
@@ -34,8 +15,7 @@ public class Student {
     public void setKlass(int klass) {
         Klass = klass;
     }
-
-    String introduce()
+    public String introduce()
     {
         return "I am a Student. I am at Class "+Klass+".";
     }
--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,31 +1,13 @@
 package practice03;
 
-public class Worker {
-    private String name;
-    private int age;
+public class Worker extends Person {
+
 
     public Worker(String name, int age) {
-        this.name = name;
-        this.age = age;
+        super(name, age);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    String introduce() {
+    public String introduce() {
         return "I am a Worker. I have a job.";
     }
 }
