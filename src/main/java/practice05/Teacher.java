a523f248d245633e6865bb342511a2285cca51ca
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-04 17:41:41 +0800

    [practice05_05][Kevin]feat: create introduce in Teacher.java

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,29 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int Klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        Klass = klass;
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String prefix = super.introduce()+" I am a Teacher. ";
+        if(Klass!=0)
+            return prefix+"I teach Class "+Klass+".";
+        else
+            return prefix+"I teach no Class";
+
+    }
+
 }
