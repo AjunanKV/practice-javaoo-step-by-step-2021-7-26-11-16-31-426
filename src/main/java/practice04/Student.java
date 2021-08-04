497d1be6a9418eb803e7197da98932681ddb758e
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-04 17:20:01 +0800

    [practice04_03][Kevin]feat:construct the student.java

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,23 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    private int Klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        Klass = klass;
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
+
+//    @Override
+//    public String introduce() {
+//        return super.introduce()+"Class "+Klass+".";
+//    }
 }
