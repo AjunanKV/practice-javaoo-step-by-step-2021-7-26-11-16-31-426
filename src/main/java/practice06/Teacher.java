398526a7607360e51ddf8c6f6a18e42bbdde0b7e
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-04 21:41:58 +0800

    [practice06_10][Kevin]feat:introducewith

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,7 +1,14 @@
 package practice06;
 
+import java.text.MessageFormat;
+import java.util.Objects;
+
 public class Teacher extends Person{
     private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name,age);
+    }
 
     public Teacher(String name, int age, Klass klass) {
         super(name, age);
@@ -18,7 +25,15 @@ public class Teacher extends Person{
 
     @Override
     public String introduce() {
-        return super.introduce()+" I am a Teacher. I teach "+klass.getDisplayName()+".";
+        if(klass!=null)
+            return MessageFormat.format("{0} I am a Teacher. I teach Class {1}.",super.introduce(),klass.getNumber());
+        else
+            return MessageFormat.format("{0} I am a Teacher. I teach No Class.",super.introduce());
+    }
+
+    public String introduceWith(Student student) {
+        return MessageFormat.format("{0} I am a Teacher. I {1}{2}.", super.introduce(),
+                ((student.getKlass().getNumber()==klass.getNumber())?"teach ":"don't teach "),student.getName());
     }
 }
 
