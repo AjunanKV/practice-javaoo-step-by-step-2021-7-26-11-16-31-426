a650f116d7bc1a7069db58e1b4029a2d484776a3
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-04 16:29:17 +0800

    [practice04_01][Kevin]feat: initialize variables in Person.java, create constuctor and create getter and setters

--- src/test/java/practice04/Practice04Test.java
+++ src/test/java/practice04/Practice04Test.java
@@ -1,16 +1,17 @@
-//package practice04;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//public class Practice04Test {
-//    @Test
-//    public void should_person_have_name_and_age() throws Exception {
-//        Person person = new Person("Tom", 21);
-//        assertThat(person.getName()).isEqualTo("Tom");
-//        assertThat(person.getAge()).isEqualTo(21);
-//    }
+package practice04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Practice04Test {
+    @Test
+    public void should_person_have_name_and_age() throws Exception {
+        Person person = new Person("Tom", 21);
+        assertThat(person.getName()).isEqualTo("Tom");
+        assertThat(person.getAge()).isEqualTo(21);
+    }
+}
 //
 //    @Test
 //    public void should_person_have_an_introduce_method_which_introduce_person_with_name_and_age() throws Exception {
