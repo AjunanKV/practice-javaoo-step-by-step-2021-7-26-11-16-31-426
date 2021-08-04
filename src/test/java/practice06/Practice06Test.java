5a8ad6ca2e409603c1a2e1c40734427607e000e0
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-04 19:01:54 +0800

    [practice06_02][Kevin]feat:build person.java

--- src/test/java/practice06/Practice06Test.java
+++ src/test/java/practice06/Practice06Test.java
@@ -14,12 +14,12 @@ public class Practice06Test {
         klass = new Klass(2);
     }
 
-//    @Test
-//    public void should_person_have_name_and_age() throws Exception {
-//        Person person = new Person("Tom", 21);
-//        assertThat(person.getName()).isEqualTo("Tom");
-//        assertThat(person.getAge()).isEqualTo(21);
-//    }
+    @Test
+    public void should_person_have_name_and_age() throws Exception {
+        Person person = new Person("Tom", 21);
+        assertThat(person.getName()).isEqualTo("Tom");
+        assertThat(person.getAge()).isEqualTo(21);
+    }
 //
 //    @Test
 //    public void should_person_have_an_introduce_method_which_introduce_person_with_name_and_age() throws Exception {
