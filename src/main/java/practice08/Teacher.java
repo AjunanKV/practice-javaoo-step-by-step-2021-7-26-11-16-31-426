bf05b9f4cd3bef64f5243b2daba8635747471f73
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-05 08:57:31 +0800

    [practice08_12][Kevin]feat:build Teacher.java

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,33 @@
 package practice08;
 
-public class Teacher {
+import java.text.MessageFormat;
+
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+//    @Override
+//    public String introduce() {
+//        if(klass!=null)
+//            return MessageFormat.format("{0} I am a Teacher. I teach Class {1}.",super.introduce(),klass.getNumber());
+//        else
+//            return MessageFormat.format("{0} I am a Teacher. I teach No Class.",super.introduce());
+//    }
+//
+//    public String introduceWith(Student student) {
+//        return MessageFormat.format("{0} I am a Teacher. I {1}{2}.", super.introduce(),
+//                ((student.getKlass().getNumber()==klass.getNumber())?"teach ":"don't teach "),student.getName());
+//    }
 }
