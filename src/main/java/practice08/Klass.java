d415812c6f95968707fa81df5fdf9245494d6c42
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-05 08:45:03 +0800

    [practice08_01][Kevin]feat:create klass.java

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,41 @@
 package practice08;
 
+import java.util.LinkedList;
+
 public class Klass {
+    private int Klass;
+    private Student leader;
+    private LinkedList<Student> members;
+
+    public Klass(int klass) {
+        Klass = klass;
+//        members = new LinkedList<Student>();
+    }
+
+//    public int getNumber() {
+//        return Klass;
+//    }
+//    public String getDisplayName() {
+//
+//        return "Class "+Klass;
+//    }
+//
+//    public void assignLeader(Student leader) {
+//        this.leader = leader;
+//        if(leader.getKlass().getNumber()!=this.Klass) {
+//            System.out.println("It is not one of us.\n");
+//        }
+//    }
+//    public Student getLeader() {
+//        return leader;
+//    }
+//    public void appendMember(Student member) {
+//        if(member.getKlass().getNumber()==this.Klass)
+//        {
+//            members.add(member);
+//        }
+//    }
+
+
+
 }
--- src/test/java/practice08/Practice08Test.java
+++ src/test/java/practice08/Practice08Test.java
@@ -1,23 +1,23 @@
-//package practice08;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.ByteArrayOutputStream;
-//import java.io.PrintStream;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//public class Practice08Test {
-//    private Klass klass;
-//    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-//
-//    @BeforeEach
-//    public void setup() {
-//        klass = new Klass(2);
-//        System.setOut(new PrintStream(outContent));
-//    }
-//
+package practice08;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Practice08Test {
+    private Klass klass;
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setup() {
+        klass = new Klass(2);
+        System.setOut(new PrintStream(outContent));
+    }
+
 //    @Test
 //    public void should_person_have_id_name_and_age() throws Exception {
 //        Person person = new Person(1, "Tom", 21);
@@ -31,19 +31,19 @@
 //        Person person2 = new Person(1, "Tom", 21);
 //        assertThat(person1).isEqualTo(person2);
 //    }
-//
+////
 //    @Test
 //    public void should_person_have_an_introduce_method_which_introduce_person_with_name_and_age() throws Exception {
 //        Person tom = new Person(1, "Tom", 21);
 //        String introduce = tom.introduce();
 //        assertThat(introduce).isEqualTo("My name is Tom. I am 21 years old.");
 //    }
-//
+////
 //    @Test
 //    public void should_class_have_a_number() throws Exception {
 //        assertThat(klass.getNumber()).isEqualTo(2);
 //    }
-//
+////
 //    @Test
 //    public void should_class_get_display_name() throws Exception {
 //        assertThat(klass.getDisplayName()).isEqualTo("Class 2");
@@ -62,7 +62,7 @@
 //    private String systemOut() {
 //        return outContent.toString();
 //    }
-//
+////
 //    @Test
 //    public void should_class_assign_a_member_student_as_leader() throws Exception {
 //        Student jerry = new Student(1, "Jerry", 8, new Klass(5));
@@ -78,13 +78,13 @@
 //        assertThat(tom.getAge()).isEqualTo(21);
 //        assertThat(tom.getKlass()).isEqualTo(klass);
 //    }
-//
+////
 //    @Test
 //    public void should_student_introduce_with_class() throws Exception {
 //        Student tom = new Student(1, "Tom", 21, klass);
 //        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am at Class 2.");
 //    }
-//
+////
 //    @Test
 //    public void should_student_introduce_itself_as_class_leader() throws Exception {
 //        Student tom = new Student(1, "Tom", 21, klass);
@@ -92,7 +92,7 @@
 //        klass.assignLeader(tom);
 //        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 2.");
 //    }
-//
+////
 //    @Test
 //    public void should_teacher_have_name_and_age_and_class() throws Exception {
 //        Teacher tom = new Teacher(1, "Tom", 21, klass);
@@ -100,19 +100,19 @@
 //        assertThat(tom.getAge()).isEqualTo(21);
 //        assertThat(tom.getKlass()).isEqualTo(klass);
 //    }
-//
+////
 //    @Test
 //    public void should_teacher_introduce_itself_with_which_class_it_teaches() throws Exception {
 //        Teacher tom = new Teacher(1, "Tom", 21, klass);
 //        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.");
 //    }
-//
+////
 //    @Test
 //    public void should_teacher_introduce_itself_with_no_class_teaching() throws Exception {
 //        Teacher tom = new Teacher(1, "Tom", 21);
 //        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.");
 //    }
-//
+////
 //    @Test
 //    public void should_teacher_introduce_a_student_it_teaches() throws Exception {
 //        Teacher tom = new Teacher(1, "Tom", 21, new Klass(1));
@@ -126,4 +126,4 @@
 //        Student jerry = new Student(1, "Jerry", 8, new Klass(2));
 //        assertThat(tom.introduceWith(jerry)).isEqualTo("My name is Tom. I am 21 years old. I am a Teacher. I don't teach Jerry.");
 //    }
-//}
+}
