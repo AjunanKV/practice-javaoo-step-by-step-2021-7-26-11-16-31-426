5c458e3558c08706d25e0087b91befd3f136c388
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-06 19:18:16 +0800

    [practice09][Kevin]feat: add implementation to: should_teacher_introduce_itself_with_which_classes_it_teaches()

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,7 +1,9 @@
 package practice09;
 
+import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.stream.Collectors;
 
 public class Teacher extends Person{
     private LinkedList<Klass> klasses;
@@ -12,5 +14,16 @@ public class Teacher extends Person{
 
     public LinkedList<Klass> getClasses() {
         return klasses;
+    }
+    @Override
+    public String introduce(){
+        return klasses !=null?
+                MessageFormat.format("{0} I am a Teacher. I teach Class {1}.",
+                        super.introduce(),
+                        klasses
+                                .stream()
+                                .map(klassValue-> String.valueOf(klassValue.getNumber()))
+                                .collect(Collectors.joining(", "))):
+                (super.introduce()+ " I am a Teacher. I teach No Class.");
     }
 }
