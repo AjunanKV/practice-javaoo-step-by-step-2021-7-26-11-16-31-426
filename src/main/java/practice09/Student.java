4083e6377762eb7dc1ca21fe9a0e39feabb30402
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-06 19:02:44 +0800

    [practice09][Kevin]feat: add implementation to: should_student_have_name_age_and_class_number()

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,27 @@
 package practice09;
 
-public class Student {
+import java.text.MessageFormat;
+
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+    //    @Override
+//    public String introduce() {
+//        return MessageFormat.format("{0} I am a Student. I am {1}{2}.",
+//                super.introduce(),
+//                ((klass.getLeader() != null && klass.getLeader().getName().equals(super.getName()))?"Leader of Class ":"at Class "),
+//                klass.getNumber());
+//    }
 }
