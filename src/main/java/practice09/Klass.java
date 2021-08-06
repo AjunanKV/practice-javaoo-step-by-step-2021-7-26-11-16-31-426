d78f6516ee50881f9ec95816245685377d8ba605
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-06 18:24:39 +0800

    [practice09][Kevin]feat: add getNumber function in Klass.java

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,39 @@
 package practice09;
 
+import java.util.LinkedList;
+
 public class Klass {
+    private int Klass;
+//    private Student leader;
+//    private LinkedList<Student> members;
+
+    public Klass(int klass) {
+        Klass = klass;
+    }
+
+    public int getNumber() {
+        return Klass;
+    }
+//
+//    public String getDisplayName() {
+//        return "Class "+Klass;
+//    }
+
+//    public void assignLeader(Student leader) {
+//        this.leader = leader;
+//        if(leader.getKlass().getNumber()!=this.Klass) {
+//            System.out.println("It is not one of us.");
+//        }
+//        }
+//
+//    public Student getLeader() {
+//        return leader;
+//    }
+//
+//    public void appendMember(Student member) {
+//        if(member.getKlass().getNumber()==this.Klass)
+//        {
+//            members.add(member);
+//        }
+//    }
 }
